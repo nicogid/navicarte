03c0d07df1bf5a91af64a3b5331d2215201da28b
Your Name <you@example.com>
2016-04-30 05:41:45 +0200

    gtcp

--- form.php
+++ form.php
@@ -5,23 +5,23 @@
 </head>
 <body>
 	<form method="post" enctype="multipart/form-data">
-		<input type="file" name="my_file[]" multiple>
+		<input type="file" name="fichiers[]" multiple>
 		<input type="submit" value="Upload">
 	</form>
 	<?php
-	if (isset($_FILES['my_file'])) {
-		$myFile = $_FILES['my_file'];
-		$fileCount = count($myFile["name"]);
+	if (isset($_FILES['fichiers'])) {
+		$fichiers = $_FILES['fichiers'];
+		$fileCount = count($fichiers["name"]);
 
 		for ($i = 0; $i < $fileCount; $i++) {
 			?>
 			<p>File #<?= $i+1 ?>:</p>
 			<p>
-				Name: <?= $myFile["name"][$i] ?><br>
-				Temporary file: <?= $myFile["tmp_name"][$i] ?><br>
-				Type: <?= $myFile["type"][$i] ?><br>
-				Size: <?= $myFile["size"][$i] ?><br>
-				Error: <?= $myFile["error"][$i] ?><br>
+				Name: <?= $fichiers["name"][$i] ?><br>
+				Temporary file: <?= $fichiers["tmp_name"][$i] ?><br>
+				Type: <?= $fichiers["type"][$i] ?><br>
+				Size: <?= $fichiers["size"][$i] ?><br>
+				Error: <?= $fichiers["error"][$i] ?><br>
 			</p>
 			<?php
 		}
