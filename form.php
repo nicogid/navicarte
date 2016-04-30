090e61129d3f46d6ea5239ef013bbd9305710b45
Your Name <you@example.com>
2016-04-30 06:12:57 +0200

    gtcp

--- form.php
+++ form.php
@@ -3,12 +3,12 @@ if (isset($_POST['new']))
 {
 	if (isset($_FILES['fichiers']))
 	{
+
 		$fichiers = $_FILES['fichiers'];
 		$fileCount = count($fichiers["name"]);
 
 		for ($i = 0; $i < $fileCount; $i++)
 		{
-			$i++;
 			$filename = hash_file('sha512', $fichiers["tmp_name"][$i]);
 			$path = "/images/campagne/".$user.".".$filename.".".pathinfo($fichiers["tmp_name"][$i], PATHINFO_EXTENSION);
 			$photo_json[$i] = $path;
@@ -25,11 +25,10 @@ if (isset($_POST['new']))
 	<form method="post" enctype="multipart/form-data">
 		<br /><input type="text" name="name" placeholder="Nom campagne">
 		<br /><textarea name="texte1" placeholder="Texte 1"></textarea>
-		<br /><textarea name="texte2" placeholder="Texte 2"></textarea>
 		<br /><textarea name="video" placeholder="youtube url separe par des virgules"></textarea>
 		<br /><input type="hidden" name="new" value="upload"/>
 		<br /><input type="file" name="fichiers[]" multiple>
-		<br /><input type="submit" value="Upload">
+		<br /><input type="submit" value="Envoyer">
 	</form>
 
 </body>
