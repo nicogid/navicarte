4949d2e7a96e401ef8a8440ba5b8e24da6a9aafd
Sylvestre <msylvestreets@yahoo.fr>
2016-04-30 10:49:39 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$user = "usertest";
-$redis = new Redis();
-$redis->connect('127.0.0.1'); // port 6379 by default
-$campagne_id = 1;
+  $user = "usertest";
+  $redis = new Redis();
+  $redis->connect('127.0.0.1'); // port 6379 by default
+  $campagne_id = 1;
 ?>
