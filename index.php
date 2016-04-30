a458713dc37fda054793cc606252bb8bfc16b753
Your Name <you@example.com>
2016-04-30 03:56:32 +0200

    gtcp

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   $redis = new Redis();
   $redis->connect('127.0.0.1'); // port 6379 by default
-  $valeur = $redis->Get('test2:ddsfds');
+  $valeur = $redis->Get('test2:ddsfds')
   echo "Ceci est un test";
   echo $valeur;
  ?>
