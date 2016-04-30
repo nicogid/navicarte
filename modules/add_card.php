0ee5fac5cb021d7cf5a01fdef837000dcf8d83b0
Your Name <you@example.com>
2016-04-30 10:17:51 +0200

    gtcp

--- modules/add_card.php
+++ modules/add_card.php
@@ -1,41 +1,41 @@
 <!-- Contact -->
-	<article id="contact" class="panel">
-		<header>
-			<h2>Ajouter une carte</h2>
-		</header>
-		<form action="#" method="post">
-			<div>
-				<div class="row">
-					<div class="6u 12u$(mobile)">
+<article id="contact" class="panel">
+	<header>
+		<h2>Ajouter une carte</h2>
+	</header>
+	<?php
+	if ($_GET['collection'] == "null")
+	{
+		echo "<h4>Merci de renseigner une campagne</h4>";
+	}
+	?>
+	<form action="#" method="post">
+		<div>
+			<div class="row">
+				<div class="6u 12u$(mobile)">
 					<input type="number" placeholder="Numero" name="id_carte"/>
-					</div>
-					<div class="6u$ 12u$(mobile)">
-						<select name="collection">
-							<option value="null">Selectionner une campagne</option>
-							<?php
-							$campagne_list = $redis->sMembers('user:'.$user.':campagne_list');
-							foreach ($campagne_list as $value)
-							{
-								echo "<option value=\"".$value."\">".$redis->hGet('campagne:'.$value, 'name')."</option>";
-							}
-							?>
-						</select>
-					</div>
-					<div class="12u$">
-						<input type="submit" value="Send Message" />
-					</div>
+				</div>
+				<div class="6u$ 12u$(mobile)">
+					<select name="collection">
+						<option value="null">Selectionner une campagne</option>
+						<?php
+						$campagne_list = $redis->sMembers('user:'.$user.':campagne_list');
+						foreach ($campagne_list as $value)
+						{
+							echo "<option value=\"".$value."\">".$redis->hGet('campagne:'.$value, 'name')."</option>";
+						}
+						?>
+					</select>
+				</div>
+				<div class="12u$">
+					<input type="submit" value="Send Message" />
 				</div>
 			</div>
-		</form>
-	</article>
+		</div>
+	</form>
+</article>
 
 
-
-if ($_GET['collection'] == "null")
-{
-	echo "<h4>Merci de renseigner une campagne</h4>";
-}
-?>
 <form>
 
 	<select name="collection">
