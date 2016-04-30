e8740db8a6ebcf5775ef181f391c158d0bffb8f0
Your Name <you@example.com>
2016-04-30 09:32:06 +0200

    gtcp

---
+++ add_card.php
@@ -0,0 +1,27 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Enrigistrer une carte</title>
+</head>
+
+<body>
+	<form>
+		<input type="number" name="id"/>
+		<select name="collection">
+			<option value="null">Selectionner une campagne</option>
+			<?php
+			$campagne_list = $redis->sMembers('user:'.$user.':campagne_list');
+			foreach ($campagne_list as $value)
+			{
+				echo "<option value=\"volvo\">Volvo</option>";
+			}
+			 ?>
+		</select>
+	</form>
+</body>
+</html>
