7d5462bb58bdfac27f3e1e2c06367d196148d4b6
Your Name <you@example.com>
2016-04-30 06:07:41 +0200

    gtcp

--- form.php
+++ form.php
@@ -1,31 +1,36 @@
-<!doctype html>
+<?php
+if (isset($_POST['new']))
+{
+	if (isset($_FILES['fichiers']))
+	{
+		$fichiers = $_FILES['fichiers'];
+		$fileCount = count($fichiers["name"]);
+
+		for ($i = 0; $i < $fileCount; $i++)
+		{
+			$i++;
+			$filename = hash_file('sha512', $fichiers["tmp_name"][$i]);
+			$path = "/images/campagne/".$user.".".$filename.".".pathinfo($fichiers["tmp_name"][$i], PATHINFO_EXTENSION);
+			$photo_json[$i] = $path;
+		}
+	}
+}
+?>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Test</title>
 </head>
 <body>
 	<form method="post" enctype="multipart/form-data">
+		<input type="text" name="name" placeholder="Nom campagne">
+		<input type="textarea" name="texte1" placeholder="Texte 1">
+		<input type="textarea" name="texte2" placeholder="Texte 2">
+		<input type="textarea" name="video" placeholder="youtube url separe par des virgules">
+		<input type="hidden" name="new" value="upload"/>
 		<input type="file" name="fichiers[]" multiple>
 		<input type="submit" value="Upload">
 	</form>
-	<?php
-	if (isset($_FILES['fichiers'])) {
-		$fichiers = $_FILES['fichiers'];
-		$fileCount = count($fichiers["name"]);
 
-		for ($i = 0; $i < $fileCount; $i++) {
-			?>
-			<p>File #<?= $i+1 ?>:</p>
-			<p>
-				Name: <?= $fichiers["name"][$i] ?><br>
-				Temporary file: <?= $fichiers["tmp_name"][$i] ?><br>
-				Type: <?= $fichiers["type"][$i] ?><br>
-				Size: <?= $fichiers["size"][$i] ?><br>
-				Error: <?= $fichiers["error"][$i] ?><br>
-			</p>
-			<?php
-		}
-	}
-	?>
 </body>
 </html>
