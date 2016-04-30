389b19f03ee3f48c31604dd618365a8f1fc5d6db
nicogid <nicolas.gidon@gmail.com>
2016-04-30 12:45:06 +0200

    index

--- index.php
+++ index.php
@@ -25,6 +25,9 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
 		</header>
 
 		<div>
+			<h1>HTML Ipsum Presents</h1>
+
+<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
 			<a href="#banner" class="button style2 scrolly-middle">Créer ma carte</a>
 		</div>
 
